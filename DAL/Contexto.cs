472f54a1e1fa665fabde7321ce253bf0846e84ba
Anthony Junior Gonzalez Hernandez <anthony_gonzalez1@ucne.edu.do>
2019-02-03 15:41:46 -0400

    Add project files.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Cuentas> Categorias { get; set; }
+        public DbSet<Depositos> TiposEgresos { get; set; }
+        public Contexto() : base("ConStr")
+        {
+
+        }
+    }
+}
---
+++ Entities/Cuentas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class Cuentas
+    {
+        [Key]
+        public int CuentaBancariaId { get; set; }
+
+        public DateTime Fecha { get; set; }
+
+        public string Nombre { get; set; }
+
+        public int Balance { get; set; }
+
+        public virtual List<Depositos> Detalle { get; set; }
+
+
+        public Cuentas()
+        {
+            this.Detalle = new List<Depositos>();
+        }
+
+        public void AgregarDetalle(int DepositoId, DateTime Fecha, int CuentaId, string Concepto, int Monto)
+        {
+            this.Detalle.Add(new Depositos(DepositoId, Fecha, CuentaId, Concepto, Monto));
+        }
+
+    }
+}
