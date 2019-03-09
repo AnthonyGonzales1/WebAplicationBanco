ff37a3027e9e465cc2638a83258ff5ff0bdc0b95
Anthony Junior Gonzalez Hernandez <anthony_gonzalez1@ucne.edu.do>
2019-03-08 20:52:12 -0400

    comienzo

---
+++ Entitiess/Cuotas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class Cuotas
+    {
+        [Key]
+        public int Id { get; set; }
+        public int PrestamoId { get; set; }
+        public decimal Capital { get; set; }
+        public decimal Interes { get; set; }
+        public decimal Cuota { get; set; }
+        public decimal Balance { get; set; }
+
+        public Cuotas()
+        {
+            Id = 0;
+            PrestamoId = 0;
+            Capital = 0;
+            Interes = 0;
+            Cuota = 0;
+            Balance = 0;
+        }
+
+        public Cuotas(int id, int prestamoId, decimal capital, decimal interes, decimal cuota, decimal balance)
+        {
+            Id = id;
+            PrestamoId = prestamoId;
+            Capital = capital;
+            Interes = interes;
+            Cuota = cuota;
+            Balance = balance;
+        }
+    }
+}
---
+++ Entitiess/Prestamos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    [Serializable]
+    public class Prestamos
+    {
+        [Key]
+        public int PrestamoId { get; set; }
+        public int CuentaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public float Interes { get; set; }
+        public int Tiempo { get; set; }
+        public decimal Capital { get; set; }
+        public float InteresTotal { get; set; }
+        public decimal Monto { get; set; }
+        public virtual List<Cuotas> Detalle { get; set; }
+
+        public Prestamos()
+        {
+            PrestamoId = 0;
+            CuentaId = 0;
+            Fecha = DateTime.Now;
+            Interes = 0;
+            Tiempo = 0;
+            Monto = 0;
+            Detalle = new List<Cuotas>();
+        }
+
+        public Prestamos(int id, int cuentaId, DateTime fecha, decimal capital, float interes, int tiempo, decimal monto)
+        {
+            PrestamoId = id;
+            CuentaId = cuentaId;
+            Fecha = fecha;
+            Interes = interes;
+            Tiempo = tiempo;
+            Monto = monto;
+            Detalle = new List<Cuotas>();
+        }
+
+        public Prestamos(int id, int cuentaId, DateTime fecha, decimal capital, float interes, int tiempo, decimal capitaT, decimal monto, List<Cuotas> detalle)
+        {
+            PrestamoId = id;
+            CuentaId = cuentaId;
+            Fecha = fecha;
+            Capital = capital;
+            Interes = interes;
+            Tiempo = tiempo;
+            Monto = Monto;
+            Detalle = detalle;
+        }
+
+        public void AddCuota(int prestamoId, int nopago, decimal capital, decimal interes, decimal cuota, decimal balance)
+        {
+            Detalle.Add(new Cuotas(0, prestamoId, nopago, capital, interes, cuota, balance));
+        }
+
+        public void AddCuota(string prestamoId, string nopago, string capital, string interes, string cuota, string balance)
+        {
+
+            Detalle.Add(new Cuotas(
+                0,
+                int.Parse(prestamoId),
+                int.Parse(nopago),
+                decimal.Parse(capital),
+                decimal.Parse(interes),
+                decimal.Parse(cuota),
+                decimal.Parse(balance)));
+        }
+
+        public void AddCuota(Cuotas cuota)
+        {
+            Detalle.Add(cuota);
+        }
+    }
+}
